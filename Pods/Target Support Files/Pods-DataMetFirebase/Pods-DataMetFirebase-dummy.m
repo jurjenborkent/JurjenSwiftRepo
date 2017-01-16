3ff49815910eee18b4c8be96c0802938540deb0d
jurjenborkent <jurjenborkent@gmail.com>
2017-01-16 16:36:50 +0100

    added readme and cleaned master branch

--- Pods/Target Support Files/Pods-DataMetFirebase/Pods-DataMetFirebase-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_DataMetFirebase : NSObject
-@end
-@implementation PodsDummy_Pods_DataMetFirebase
-@end
---
+++ README.md
@@ -0,0 +1,1 @@
+#Jurjens Assessment
